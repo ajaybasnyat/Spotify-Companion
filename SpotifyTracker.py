b62c7a848b5f3051029a6f53be0aad4d421f74af
ajaybasnyat <ajaybasnyat@gmail.com>
2021-03-02 22:02:50 -0600

    added methods for getting playback track and artist

--- SpotifyTracker.py
+++ SpotifyTracker.py
@@ -16,13 +16,37 @@ class APImanager:
         token = util.prompt_for_user_token(username,scope,client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI)
         self.sp = spotipy.Spotify(auth=token)
 
-
-    def getPlaying(self):
+# returns current track name
+    def getCurrentTrack(self):
         results = self.sp.current_playback()
-        track = results['item']
-        # print(track['artists'][0]['name'])
-        return track['name']
-        # time.sleep(3)
+        print(results)
+        if results is not None:
+            item = results['item']
+            return item['name']
+        else:
+            return None
 
+# returns current track artist
+    def getCurrentArtist(self):
+        results = self.sp.current_playback()
+        if results:
+            item = results['item']
+            artist = item["artists"][0]
+            return artist['name']
+        else:
+            return None
+
+# returns previous track name
+    def getPreviousTrack(self):
+        results = self.sp.current_playback()
+        if results:
+            item = results['item']
+            artist = item["artists"][0]
+            return artist['name']
+        else:
+            return None
+
+test = APImanager()
+print(test.getCurrentArtist())
 
     
